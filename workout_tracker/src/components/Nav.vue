ae50f145a0912631f3f67df60d9c0043991c376f
Olivier Poisson <70164489+olivierpoisson@users.noreply.github.com>
2022-03-02 23:24:02 -0500

    Ajout du logo dans la vue + ajouts styles

--- workout_tracker/src/components/Nav.vue
+++ workout_tracker/src/components/Nav.vue
@@ -1,7 +1,13 @@
 <template>
-    <div class="nav RowCenter">
-        <img src="../assets/Workout Tracker.png" alt="" />
-        <a href="#">Home</a>
+    <div class="nav Row">
+        <a class="logo" href="#">
+            <img src="../assets/Logo.png" alt="" />
+        </a>
+        <div class="links RowCenter">
+            <a href="#">Home</a>
+            <a href="#">Workouts</a>
+            <a href="#">Exercices</a>
+        </div>
     </div>
 </template>
 
@@ -21,8 +27,28 @@ export default {
     left: 0;
     box-shadow: 0px 5px 15px 0px #00000030;
     background-color: white;
+    padding: 0 10vw;
 }
 a {
     font-size: 20px;
+}
+.logo {
+    margin: auto 0;
+    cursor: pointer;
+    height: 100%;
+    display: flex;
+    align-items: center;
+}
+img {
+    height: 90%;
+}
+.links {
+    width: -webkit-fill-available;
+}
+.links a {
+    margin: 0px 20px;
+}
+.links a:hover {
+    text-decoration: underline 3px #5271ff;
 }
 </style>
