1485c4eb5484754222c7de1d7c1bbf1fd3c69687
Olivier Poisson <70164489+olivierpoisson@users.noreply.github.com>
2022-03-02 20:29:38 -0500

    Ajout solution de base + styles globaux de base

---
+++ workout_tracker/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+    <div id="nav">Hello World</div>
+</template>
+
+<style>
+* {
+    font-family: "Poppins", sans-serif;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+.Row {
+    display: flex;
+}
+.RowCenter {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.Col {
+    display: flex;
+    flex-direction: column;
+}
+.ColCenter {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+button {
+    padding: 1em 2em;
+}
+button:active {
+    transform: scale(0.95);
+}
+a {
+    text-decoration: none;
+    color: #2c3e50;
+}
+h1,
+h2,
+h3,
+h4,
+h5,
+a,
+p {
+    text-underline-offset: 5px;
+}
+</style>
